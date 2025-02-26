84c65bef5fa6f7b7448d3d077ed934fe14c24b8d
killersmile322 <sashagudush-2014@mail.ru>
2025-02-26 10:38:36 +0300

    Новые разделы + начало скроллов в js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("Zdarova,epta");
+const navButton = document.querySelector('.nav-btn');
+console.log(navButton);
+
+navButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log('Клик по кнопке');
+    
+});
 
