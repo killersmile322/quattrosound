27983f0054056389bd5ce7a9a8ba5fcb3d34dfcd
killersmile322 <sashagudush-2014@mail.ru>
2025-02-28 19:12:32 +0300

    Доделал скролл

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const navButton = document.querySelector('.nav-btn');
-console.log(navButton);
+const navButtons = document.querySelectorAll('.nav-btn');
+const upButton = document.querySelector('.up-button');
+// console.log(navButton)
 
-navButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    console.log('Клик по кнопке');
+
+window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+      upButton.classList.add('shown');
+    //   console.log(pageYOffset)
+    }  else {
+        upButton.classList.remove('shown');
+      }
+  };
+upButton.onclick = function () {
+window.scrollTo(0, 0);
+};
+
+
+for (let i = 0; i < navButtons.length; i++ ) {
+    navButtons[i].addEventListener('click', function() {
+        console.log('Клик по кнопке' + i);
+        if (i === 0) {
+            window.scrollTo(0, 1817.5);  
+        } else {
+            window.scrollTo(0, 3022.5);  
+        }
+    });   
+
+}
+// navButton.addEventListener('click', function (evt) {
+//     evt.preventDefault();
+//     console.log('Клик по кнопке');
+//     window.scrollTo(0, 1817.5);
     
-});
+// });
 
