dbaeed9cd41996a124ef319c3bb575219d6d72f0
killersmile322 <sashagudush-2014@mail.ru>
2025-03-09 15:18:11 +0300

    share ветки

--- data.js
+++ data.js
@@ -3,20 +3,19 @@ const songs = [
         name: 'Скелетики',
         path: 'music/Andrew Gold - Spooky scary skeletons (mlg edit)2.mp3',
         artist: 'Andrew Gold',
-        background: 'images/player/skeletons.jpg'
+        background: 'images/player/skeletons.jpg',
     },
     {
         name: 'Песня девочки',
         path: 'music/Noga Erez - Dance while You Shoot.mp3',
         artist: 'Noga Erez',
-        background: 'images/player/Noga-Erez.jpeg'
+        background: 'images/player/Noga-Erez.jpeg',
     },
     {
         name: 'НАРУТО!',
         path: 'music/Akatsuki Theme - Pain.mp3',
         artist: 'Akatsuki',
-        background: 'images/player/naruto-pain.jpg'
-
-    }
-]
+        background: 'images/player/naruto-pain.jpg',
+    },
+];
 // console.log('hihi')
--- script.js
+++ script.js
@@ -4,41 +4,35 @@ const upButton = document.querySelector('.up-button');
 
 window.onscroll = function () {
     if (window.pageYOffset > 200) {
-      upButton.classList.add('shown');
-    //   console.log(pageYOffset)
-    }  else {
+        upButton.classList.add('shown');
+        //   console.log(pageYOffset)
+    } else {
         upButton.classList.remove('shown');
-      }
-  };
+    }
+};
 upButton.onclick = function () {
-window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 };
 
-
-for (let i = 0; i < navButtons.length; i++ ) {
-    navButtons[i].addEventListener('click', function() {
-        // Поменять условие 'if' на navBtn.onclick 
+for (let i = 0; i < navButtons.length; i++) {
+    navButtons[i].addEventListener('click', function () {
+        // Поменять условие 'if' на navBtn.onclick
         if (i === 0) {
-            window.scrollTo(0, 2066.5);  
+            window.scrollTo(0, 2066.5);
         } else {
-            window.scrollTo(0, 3472.5);  
+            window.scrollTo(0, 3472.5);
         }
-    });   
-
+    });
 }
 
 // Музыкальный плеер
-
-// Спросить почему коллекция не работает из другого файла... РАБОТАЕТ(важен порядок добавления js файлов)
-
-
 let currentMusic = 0;
 
 const music = document.querySelector('#audio');
 const seekBar = document.querySelector('.seek-bar');
 const songName = document.querySelector('.song-name');
-const artistName = document.querySelector('.artist-name') ;
-const musicBackground = document.querySelector('.music-background') ;
+const artistName = document.querySelector('.artist-name');
+const musicBackground = document.querySelector('.music-background');
 // const currentTime;
 // const musicDuration;
 const playBtn = document.querySelector('.play-btn');
@@ -46,15 +40,13 @@ const forwardBtn = document.querySelector('.forward-btn');
 const backBtn = document.querySelector('.back-btn');
 
 playBtn.addEventListener('click', () => {
-    if(playBtn.className.includes('pause')){
+    if (playBtn.className.includes('pause')) {
         music.play();
     } else {
         music.pause();
     }
     // playBtn.classList.toggle('pause-btn');
     playBtn.classList.toggle('pause');
-    
-
 });
 
 const setMusic = (i) => {
@@ -71,50 +63,45 @@ const setMusic = (i) => {
     setTimeout(() => {
         seekBar.max = music.duration;
         // musicDuration.innerHTML = music.duration;
-
     }, 300);
-}
+};
 
-setMusic(0)
+setMusic(0);
 
 setInterval(() => {
     seekBar.value = music.currentTime;
-    if(music.currentTime == seekBar.max ) {
+    if (music.currentTime == seekBar.max) {
         forwardBtn.click();
     }
 }, 500);
 
 seekBar.addEventListener('change', () => {
     music.currentTime = seekBar.value;
-})
+});
 
 const playMusic = () => {
     music.play();
     playBtn.classList.remove('pause');
     // console.log(currentMusic + 'now')
     // console.log(songs.length + 'dlina')
-}
+};
 
 forwardBtn.addEventListener('click', () => {
-    if(currentMusic >= songs.length - 1) {       
+    if (currentMusic >= songs.length - 1) {
         currentMusic = 0;
     } else {
         currentMusic++;
     }
     setMusic(currentMusic);
     playMusic();
-    
-})
+});
 
 backBtn.addEventListener('click', () => {
-    if(currentMusic <= 0) {
+    if (currentMusic <= 0) {
         currentMusic = songs.length - 1;
     } else {
         currentMusic--;
     }
     setMusic(currentMusic);
     playMusic();
-})
-
-
-
+});
