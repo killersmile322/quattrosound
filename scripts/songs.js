edd41ada40c6baabc683f2870b021589d2c94c82
killersmile322 <sashagudush-2014@mail.ru>
2025-04-14 18:33:10 +0300

    video-table fixes music-player fixes

--- scripts/songs.js
+++ scripts/songs.js
@@ -1,31 +1,111 @@
 const songs = [
-    {
-        name: "Скелетики",
-        path: "music/Andrew Gold - Spooky scary skeletons (mlg edit)2.mp3",
-        artist: "Andrew Gold",
-        background: "images/player/skeletons.jpg",
+    { 
+        name: 'Струнный квартет',
+        path: 'music/Эдвард_Григ_Струнный_квартет_соль_минор_Op_27.mp3',
+        artist: 'Григ',
+        background: 'images/player/grieg.jpg',
     },
     {
-        name: "Песня девочки",
-        path: "music/Noga Erez - Dance while You Shoot.mp3",
-        artist: "Noga Erez",
-        background: "images/player/Noga-Erez.jpeg",
+        name: 'Струнный квартет',
+        path: 'music/Y2mate.mx - Alexander Borodin - String Quartet No. 1 (128 kbps).mp3',
+        artist: 'Бородин',
+        background: 'images/player/borodin.jpg',
     },
     {
-        name: "НАРУТО!",
-        path: "music/Akatsuki Theme - Pain.mp3",
-        artist: "Akatsuki",
-        background: "images/player/naruto-pain.jpg",
+        name: 'Элегическое трио',
+        path: 'music/Y2mate.mx - Rachmaninov - Trio élégiaque no. 2, op. 9 (Audio+Sheet) [Kogan_Luzanov_Svetlanov] (128 kbps).mp3',
+        artist: 'Рахманинов',
+        background: 'images/player/Raxmaninov.jpg',
     },
 ];
 
+let currentMusic = 0;
+
+const music = document.querySelector('#audio');
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector('.song-name');
+const artistName = document.querySelector('.artist-name');
+const musicBackground = document.querySelector('.music-background');
+// const currentTime;
+// const musicDuration;
+const playBtn = document.querySelector('.play-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const backBtn = document.querySelector('.back-btn');
+
+playBtn.addEventListener('click', () => {
+    if (playBtn.className.includes('pause')) {
+        music.play();
+    } else {
+        music.pause();
+    }
+    // playBtn.classList.toggle('pause-btn');
+    playBtn.classList.toggle('pause');
+});
+
+const setMusic = (i) => {
+    // console.log(currentMusic)
+    seekBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    musicBackground.style.backgroundImage = `url('${song.background}')`;
+
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        // musicDuration.innerHTML = music.duration;
+    }, 300);
+};
+
+setMusic(0);
+
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    if (music.currentTime == seekBar.max) {
+        forwardBtn.click();
+    }
+}, 500);
+
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
+});
+
+const playMusic = () => {
+    music.play();
+    playBtn.classList.remove('pause');
+    // console.log(currentMusic + 'now')
+    // console.log(songs.length + 'dlina')
+};
+
+forwardBtn.addEventListener('click', () => {
+    if (currentMusic >= songs.length - 1) {
+        currentMusic = 0;
+    } else {
+        currentMusic++;
+    }
+    setMusic(currentMusic);
+    playMusic();
+});
+
+backBtn.addEventListener('click', () => {
+    if (currentMusic <= 0) {
+        currentMusic = songs.length - 1;
+    } else {
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playMusic();
+});
+
 const interactiveSongsContainer = document.getElementById(
-    "interactive-songs-container"
+    'interactive-songs-container'
 );
 const fillInteractoveSongsContainer = () => {
     songs.forEach((song, index) => {
-        const songItem = document.createElement("div");
-        songItem.classList.add("song-item");
+        const songItem = document.createElement('div');
+        songItem.classList.add('song-item');
         songItem.innerHTML = `
             <span class="song-item-index">${index + 1}</span>
             <img src="${song.background}" />
@@ -34,13 +114,15 @@ const fillInteractoveSongsContainer = () => {
         `;
         songItem.onclick = (e) => {
             setMusic(index);
-            if (playBtn.className.includes("pause")) {
+            if (playBtn.className.includes('pause')) {
                 music.play();
             } else {
                 music.pause();
             }
             // playBtn.classList.toggle('pause-btn');
-            playBtn.classList.toggle("pause");
+            
+            // playMusic();
+            playBtn.classList.toggle('pause');
         };
         interactiveSongsContainer?.appendChild(songItem);
     });
